a66c19e77e31d87715e3d8b87982555c9ebb2e34
Carrie Jeanne <cdonica@ufl.edu>
2016-08-25 18:52:34 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,39 +1,30 @@
 /*global describe, it */
 
-describe('Fix the Scope', function() {
-  describe('myAnimal()', function() {
-    it('returns my animal', () => {
-      expect(window.myAnimal()).toEqual('dog')
-    })
-  })
 
-  describe('yourAnimal()', function() {
-    it('returns your animal', function(){
-      expect(window.yourAnimal()).toEqual('cat')
-    })
 
-    it('does not hard-code the answer', function() {
-      expect(window.yourAnimal.toString()).toNotContain("return 'cat'")
-    })
-  })
+function myAnimal() {
+  return 'dog'
+}
 
-  describe('add2(n)', function() {
-    it('adds two to n', function() {
-      const n = Math.floor(Math.random() * 1000)
-      expect(window.add2(n)).toEqual(n + 2)
-    })
-  })
 
-  describe('funkyFunction()', function() {
-    it('returns a function', function() {
-      expect(typeof window.funkyFunction()).toEqual('function')
-    })
-  })
 
-  describe('theFunk', function() {
-    it('is "FUNKY!"', function() {
-      expect(window.theFunk).toEqual('FUNKY!')
-    })
-  })
+function yourAnimal() {
+  return 'cat'
+}
 
-})
+
+
+
+function add2(n) {
+  return (n + 2)
+}
+
+
+function funkyFunction() {
+  return ('function')
+}
+
+
+function theFunk () {
+  return 'FUNKY!'
+}
