6d092ca079d0fca6f91ee7684808a411d7c53204
imani fecu <imani.fecu72@ymail.com>
2016-08-27 19:53:01 +0000

    Done.

--- index.js
+++ index.js
@@ -4,25 +4,29 @@ function myAnimal() {
   return animal
 }
 
-function yourAnimal() {
-  // How can we make sure that this function
-  // and the above function both pass?
-  // P.S.: You can't just hard-code 'cat' below
+function yourAnimal(){
+  animal="cat"
   return animal
 }
 
 function add2(n) {
+  const two = 2
   return n + two
 
   // Feel free to move things around!
-  const two = 2
+
 }
 
-var funkyFunction = function() {
-  return function() {
-    return "FUNKY!"
+function funkyFunction ()
+{
+  var funk = function funky()
+  function funky()
+  {
+    return "Funky!"
   }
-}
+    return var funk
+  }
+
 
 // We want this to return a function that returns "FUNKY!" -- how can we accomplish that?
-var theFunk = funkyFunction
+console.log(funkyFunction())
