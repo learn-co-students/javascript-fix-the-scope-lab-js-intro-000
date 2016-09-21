49a948fa39e669822e1debbe21f9bf13948fdba2
Adam Nur <anur@comcast.net>
2016-09-21 01:17:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-var animal = 'dog'
+var animal='cat'
+
+
+
 
 function myAnimal() {
+  var animal='dog'
   return animal
 }
 
@@ -12,17 +16,19 @@ function yourAnimal() {
 }
 
 function add2(n) {
+  const two = 2
   return n + two
 
   // Feel free to move things around!
-  const two = 2
+
 }
 
 var funkyFunction = function() {
-  return function() {
+  return function funky() {
     return "FUNKY!"
   }
 }
 
 // We want this to return a function that returns "FUNKY!" -- how can we accomplish that?
-var theFunk = funkyFunction
+var theFunk = funkyFunction()
+theFunk=theFunk()
