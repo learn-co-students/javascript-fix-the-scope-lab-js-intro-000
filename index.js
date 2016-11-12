63a1b27b1df2f16dba33393be1f5eab3dd6ccf34
Anna Brakowska <anna.brakowska91@gmail.com>
2016-11-12 22:17:06 +0000

    Done.

--- index.js
+++ index.js
@@ -13,11 +13,12 @@ var animal = 'cat'
   return animal
 }
 
+
+  const two = 2
 function add2(n) {
   return n + two
 
   // Feel free to move things around!
-  const two = 2
 }
 
 var funkyFunction = function() {
