92ce774ea352529c8ca88759973a0d6969c1398b
Abdullah Alam <abdullah.alam.omi@gmail.com>
2017-02-15 23:04:12 +0000

    Done.

--- index.js
+++ index.js
@@ -8,14 +8,17 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
+  // Fix the Scope yourAnimal() returns your animal
+  var animal = 'cat'
   return animal
 }
 
-function add2(n) {
-  return n + two
 
+function add2(n) {
   // Feel free to move things around!
+  // Fix the Scope add2(n) adds two to n
   const two = 2
+  return n + two
 }
 
 var funkyFunction = function() {
@@ -26,4 +29,5 @@ var funkyFunction = function() {
 
 // We want 'funkyFunction' on the line below to return a function that returns "FUNKY!" -- how can we accomplish that?
 // NOTE: To pass this final test, you only need to modify the code below this line.
-var theFunk = funkyFunction
+// Fix the Scope theFunk is "FUNKY!"
+var theFunk = funkyFunction()()
