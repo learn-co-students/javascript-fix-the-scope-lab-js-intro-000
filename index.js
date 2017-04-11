5abe40613a9c29524bcda6c62b6e086a30ca591c
Kraig Tapang <kraig.tapang@gmail.com>
2017-04-11 17:56:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,29 +1,42 @@
 var animal = 'dog'
 
-function myAnimal() {
-  return animal
+function myAnimal(n) {
+  n = animal;
+    return animal;
 }
 
-function yourAnimal() {
+function yourAnimal(n) {
+  var animal = 'cat';
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
-  return animal
+  return animal;
 }
 
 function add2(n) {
-  return n + two
-
-  // Feel free to move things around!
   const two = 2
+  return n + two
 }
 
-var funkyFunction = function() {
-  return function() {
+/*
+  This below can hurt your brain.
+  This is basically a function returning a function.
+  Why is it confusing?
+  Well think of Inception, and having a dream within a dream?
+  Sort of hard to wrap your mind around that right?!
+  This takes time to understand but the more you see it
+  the concept will eventually click over time.
+*/
+
+var funkyFunction = function()
+{
+  return function()
+    {
     return "FUNKY!"
-  }
+    }
 }
 
 // We want 'funkyFunction' on the line below to return a function that returns "FUNKY!" -- how can we accomplish that?
 // NOTE: To pass this final test, you only need to modify the code below this line.
-var theFunk = funkyFunction
+
+var theFunk = funkyFunction()();
