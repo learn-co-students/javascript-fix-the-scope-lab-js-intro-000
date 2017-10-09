93c7bdff16141ea9ab58af5b82f6856d6a1da4c0
Aaron R. Trevino <aaronrtrevino12@gmail.com>
2017-10-09 10:24:44 +0000

    Done.

--- index.js
+++ index.js
@@ -5,19 +5,22 @@ function myAnimal() {
 }
 
 function yourAnimal() {
-  var animal = 'cat'
+  var animal = 'cat'  // returns local variable 'cat' and not global variable 'dog'
   return animal
 }
 
 function add2(n) {
-  const two = 2
+  const two = 2  // defined variable before addition
   return n + two
 }
 
-var theFunk = funkyFunction()()
-
-function funkyFunction() {
-  return function() {
-    return "FUNKY!"
+var funkyFunction = function() {  // theFunk or funkyFunction - returns outer function
+  return function() {  // theFunk or funkyFunction() - returns inner function
+    return "FUNKY!"  // theFunk of funkyFunction()() - returns "FUNKY!"
   }
 }
+
+// How can we return "FUNKY!"?
+// var theFunk = funkyFunction  <-- this returns function without running it
+// var theFunk = funkyFunction()  <-- this runs function but only returns the second function "function()"
+var theFunk = funkyFunction()()  // this returns "FUNKY!"
