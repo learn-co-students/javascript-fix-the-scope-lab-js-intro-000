b9400ee84b75cf8cbb498d92bf2d902ff38c90a8
Mary Jane Phifer <mjphifer@gmail.com>
2019-02-28 22:41:17 +0000

    Done.

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
-  var animal = 'cat'
+  var yourAnimal = 'cat'
+  return yourAnimal
 }
 
 function add2(n) {
