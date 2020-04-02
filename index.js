85e5d8e7749714a694ec01730f860172f00a64ce
ivang <silasevan@gmail.com>
2020-04-02 01:17:36 +0000

    Done.

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ function myAnimal() {
 }
 
 function yourAnimal() {
+  var animal="cat"
   // The tests expect this function to return `animal` just like the previous function
-  // However, you cannot simply modify the existing variable declared on line 1 in the global scope 
+  // However, you cannot simply modify the existing variable declared on line 1 in the global scope
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: Hard-coding 'cat' below will not work
@@ -15,14 +16,18 @@ function yourAnimal() {
 }
 
 function add2(n) {
-  return n + two
+const two = 2
+  return n +=two
+
 
   // Feel free to move things around!
-  const two = 2
+
 }
 
 var funkyFunction = function() {
+  var = "FUNKY";
   return function() {
+
     return "FUNKY!"
   }
 }
