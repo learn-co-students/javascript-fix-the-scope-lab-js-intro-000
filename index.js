974b034e9063350dc45825632ae8ed62c4814ab3
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-04-24 02:32:08 +0000

    Pass scope lab

--- index.js
+++ index.js
@@ -31,4 +31,5 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction()
+var lilFunk = funkyFunction()
+var theFunk = lilFunk()
