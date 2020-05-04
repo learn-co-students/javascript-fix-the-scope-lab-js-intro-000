8f82437a424dc25c3e76335be9ac9db42a4a8112
Shahad Puthukudi <shahadp@gmail.com>
2020-05-04 17:54:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 var animal = 'dog'
-
 function myAnimal() {
   // You should not need to modify this function
   return animal
 }
 
-animal='cat'
 function yourAnimal() {
+  var animal='cat'
   // The tests expect this function to return `animal` just like the previous function
   // However, you cannot simply modify the existing variable declared on line 1 in the global scope
   // How can we make sure that this function
@@ -16,10 +15,11 @@ function yourAnimal() {
 }
 
 function add2(n) {
+  const two = 2
   return n + two
 
   // Feel free to move things around!
-  const two = 2
+
 }
 
 var funkyFunction = function() {
@@ -30,4 +30,5 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
+var test=funkyFunction()
+var theFunk = test()
